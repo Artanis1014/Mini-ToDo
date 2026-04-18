9135c6bd551fd3d54bd2aa39ce328d315dc56b65
Artanis <1745035571@qq.com>
2026-04-18 19:09:28 +0800

    新增删除功能

--- src/main.js
+++ src/main.js
@@ -15,15 +15,24 @@ input.addEventListener("keypress", function(e){
 function add_task(){
     const task_text = input.value.trim();
     if(task_text === ""){
+        alert("请输入任务");
         return;
     }
     const task = document.createElement("li");
+    const delete_button = document.createElement("button")
+    delete_button.className = "delete_button";
+    delete_button.textContent = "x"
+    delete_button.addEventListener("click",function(e){
+        e.stopPropagation();
+        task.remove();
+    })
     const clickbox = document.createElement("div");
     clickbox.className = "clickbox";
     const text = document.createElement("span");
     text.textContent = task_text;
     task.appendChild(clickbox);
     task.appendChild(text);
+    task.appendChild(delete_button);
     task.addEventListener("click", function(){
         task.classList.toggle("completed");
         if(task.classList.contains("completed")){
@@ -31,14 +40,13 @@ function add_task(){
         }
     })
     list.appendChild(task);
-    const taskElements = list.querySelectorAll("li");
-    if(taskElements.length > 10){
-        const completedTasks = list.querySelectorAll("li.completed");
-        if(completedTasks.length > 0){
-            completedTasks[0].remove();
+    const task_num = list.querySelectorAll("li");
+    if(task_num.length > 10){
+        const completed_tasks = list.querySelectorAll("li.completed");
+        if(completed_tasks.length > 0){
+            completed_tasks[0].remove();
         }
     }
     input.value = "";
     input.classList.remove("show");
 }
-load();
